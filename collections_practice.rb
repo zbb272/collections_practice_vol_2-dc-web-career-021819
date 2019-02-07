a23bd1fd60cfd1644a0bc33aca55cecf92293f76
Zachary <zbberman@yahoo.com>
2019-02-07 17:43:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -99,30 +99,3 @@ def organize_schools(schools)
   end 
   organized_schools
 end 
-  # let(:schools) {
-  #     {
-  #     "flatiron school bk" => {
-  #       :location => "NYC"
-  #     },
-  #     "flatiron school" => {
-  #       :location => "NYC"
-  #     },
-  #     "dev boot camp" => {
-  #       :location => "SF"
-  #     },
-  #     "dev boot camp chicago" => {
-  #       :location => "Chicago"
-  #     },
-  #     "general assembly" => {
-  #       :location => "NYC"
-  #     },
-  #     "Hack Reactor" => {
-  #       :location => "SF"
-  #     }
-  #   }
-  # }
-  # let(:organized_schools) {
-  #   {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"],
-  #   "SF"=>["dev boot camp", "Hack Reactor"],
-  #   "Chicago"=>["dev boot camp chicago"]}
-  # }
